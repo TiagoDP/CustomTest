4ed1ceb69c4776d51208e457b9d23aac22df4970
D064809 <tom.bendrath@sap.com>
2021-12-07 16:58:13 +0100

    update post method formatting

--- scopeBundleAPI.js
+++ scopeBundleAPI.js
@@ -1,6 +1,7 @@
+const axios = require('axios').default;
+
 // Additional headers can be passed as a fourth argument of ajaxPromisify.
 // The authentication credentials (technical user name & password) could potentially be set as header.
-const axios = require('axios').default;
 var ajaxPromisify = (url, type, data, headers) => {
 	return new Promise((resolve, reject) => {
 		$.ajax({
@@ -44,10 +45,10 @@ const SERVICE_END_POINT = 'https://customersuccessmarketstandard-dev-cap-sac-sco
 		 - Read all ScopeBundles: /service/ScopeBundle
 		 - Read single ScopeBundle: /service/ScopeBundle(ScopeBundleID=10005678-90ab-cdef-1234-567890abcdef)
 		 */
-		async get(path, data) {
+		async get(path, payload) {
 			const {
 				response
-			} = await ajaxPromisify(`${SERVICE_END_POINT}${path}`, 'GET', data)
+			} = await ajaxPromisify(`${SERVICE_END_POINT}${path}`, 'GET', payload)
 			return response;
 		}
 
@@ -55,53 +56,54 @@ const SERVICE_END_POINT = 'https://customersuccessmarketstandard-dev-cap-sac-sco
 		 - Create ScopeBundle: /service/ScopeBundle
 		 - Create ScopeBundleScenario: /service/ScopeBundle(ScopeBundleID=10005678-90ab-cdef-1234-567890abcdef)/_ScopeBundleScenario
 		 */
-		async post(path, data) {
+		async post(path, payload) {
 			const {
 				response
-			} = await ajaxPromisify(`${SERVICE_END_POINT}${path}`, 'POST', data)
+			} = await ajaxPromisify(`${SERVICE_END_POINT}${path}`, 'POST', payload)
 			return response.statusCode
 		}
-		async function sendPostRequest(path, payload) {
-    var responseObject;
-    try {
-        // var authString = "Basic " + Buffer.from(email + ":" + password).toString('base64');
-        const requestionOptions = {
-            method: 'POST',
-            url: SERVICE_END_POINT + path,
-            headers: {
-                'Authorization': authString,
-                'Content-type': 'application/json'
-            },
-            data: payload
-        };
 
-        const response = await axios(requestionOptions);
-        responseObject = {
-            data: response.data,
-            status: response.status
-        }
-    } catch (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log("error.response.status", error.response.status);
-            console.log("error.response.data", error.response.data);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log("error.request", error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        //console.log("error.config", error.config);
-    }
+		async postAxios(path, payload) {
+			var responseObject;
+			try {
+				// var authString = "Basic " + Buffer.from(email + ":" + password).toString('base64');
+				const requestionOptions = {
+					method: 'POST',
+					url: `${SERVICE_END_POINT}${path}`,
+					headers: {
+						//'Authorization': authString,
+						'Content-type': 'application/json'
+					},
+					data: payload
+				};
 
-    return responseObject;
-};
+				const response = await axios(requestionOptions);
+				responseObject = {
+					data: response.data,
+					status: response.status
+				}
+			} catch (error) {
+				if (error.response) {
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.log("error.response.status", error.response.status);
+					console.log("error.response.data", error.response.data);
+				} else if (error.request) {
+					// The request was made but no response was received
+					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+					// http.ClientRequest in node.js
+					console.log("error.request", error.request);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log('Error', error.message);
+				}
+				//console.log("error.config", error.config);
+			}
+
+			return responseObject;
+		};
 	}
-	
+
 
 	customElements.define('com-sap-standard-content-scopebundleapi', MainWebComponent)
 })()
